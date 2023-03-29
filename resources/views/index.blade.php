d41b52b7845b78014a2ca435c432174355c2fa85
64160292 <116263346+SuriyaRongsoongnern@users.noreply.github.com>
2023-03-29 15:29:42 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welll</title>
+    <title>BISHOPPz</title>
 </head>
 <body> 
+    <?php
+
+    $user = "BISHOPPz";
+
+    ?>
     <h1>ยินดีต้อนรับเข้าสู่เว็บไซต์</h1>
     <p>BISHOPPz - บริการจ้างเล่นเกม</p>
+    {{$user}}
+
+
 </body>
 </html>
